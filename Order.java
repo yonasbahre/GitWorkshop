1b80f6579a625ba424775661e0ddcfdb338748ba
vmpuri <vpuri@ufl.edu>
2021-01-26 17:13:33 -0500

    Reset to first commit

--- Order.java
+++ Order.java
@@ -8,16 +8,5 @@ class Order {
         System.out.println("Your total is $" + totalCost);
         System.out.println("Here are your items: " + items);
     }
-    public void shake(int num) {
-      totalCost += 12;
-      if (num == 1) {
-        items.add("Vanilla Shake");
-      }
-      else if (num ==2) {
-        items.add("Chocolate Shake");
-      }
-      else {
-        items.add("Strawberry Shake");
-      }
-    }
+
 }
